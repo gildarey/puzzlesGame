1ae830d4326c26c61764e94c0ead62262b6fa793
gildarey <gildarey.r@gmail.com>
2024-12-09 20:39:58 +0100

    Fix time out

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const axios = require('axios');
 
 const app = express();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 const HOSTNAME = 'localhost';
 const openaiApiKey = process.env.OPENAI_API_KEY;
 const openaiApiUrl = process.env.OPENAI_API_URL;
