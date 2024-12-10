ef966df0f25038a27077428270d4090b96b47152
gildarey <gildarey.r@gmail.com>
2024-12-10 12:59:44 +0100

    Adding comments and footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,11 +4,10 @@ import '../assets/Footer.css';
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>Created by GR</p>
-            <div className="footer-links">
-                <a href="https://www.linkedin.com/in/gilda-rey/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                <a href="https://github.com/gildarey/puzzlesGame" target="_blank" rel="noopener noreferrer">GitHub Project</a>
-            </div>
+            <p>Created by <a href="https://www.linkedin.com/in/gilda-rey/" target="_blank"
+                             rel="noopener noreferrer">GR</a> - <a href="https://github.com/gildarey/puzzlesGame"
+                                                                   target="_blank" rel="noopener noreferrer">GitHub
+                Project</a></p>
         </footer>
     );
 };
